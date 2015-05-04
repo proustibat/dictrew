8c75198881609e8764d6feb656a4e86435b1d980
Proustibat <jennifer.proust@gmail.com>
2015-05-04 23:38:48 +0200

    data two first chapters

--- public/js/app.js
+++ public/js/app.js
@@ -14,35 +14,107 @@ require.config({
         radio: '../bower_components/backbone.radio/build/backbone.radio'
     },
     shim: {
-        backbone: { deps: ['jquery', 'underscore'], exports: 'Backbone' },
-        underscore: { exports: '_' },
-        bootstrap: {deps: ['jquery']},
-        handlebars: { exports: 'Handlebars'},
-        tipper: { deps: ['jquery'] },
-        marionette: { deps: ["backbone"], exports:"Marionette" },
-        radio: { deps: ["backbone"], exports:"Radio" }
+        backbone: {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        underscore: {
+            exports: '_'
+        },
+        bootstrap: {
+            deps: ['jquery']
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        },
+        tipper: {
+            deps: ['jquery']
+        },
+        marionette: {
+            deps: ["backbone"],
+            exports: "Marionette"
+        },
+        radio: {
+            deps: ["backbone"],
+            exports: "Radio"
+        }
     }
 });
 
 require([
     'jquery',
-    'bootstrap'
-    ], function($, _bootstrap){
-        // this is where all the site code should begin
-        console.log("allo");
-        var app = {
-            start: function() {
-                console.log('this is :', this);
-            }
-        };
-        return app.start();
+    'bootstrap',
+    'handlebars',
+    'backbone'
+], function($, _bootstrap, Handlebars, Backbone) {
+
+    // Vue de la collection
+    var SoundsView = Backbone.View.extend({
+        initialize: function() {
+            console.log('SoundsView.initialize');
+            this.listenTo(this.collection, "sync destroy", this.render);
+            this.collection.fetch();
+        },
+        render: function() {
+            console.log('SoundsView.render');
+            var $template = "";
+            this.$el.html("");
+            this.collection.each(function(model) {
+                console.log('model : ', model);
+                var child = new SoundView({
+                    model: model
+                });
+                this.$el.append(child.el);
+            }.bind(this));
+        }
+    });
+
+    // Vue d'un son
+    var SoundView = Backbone.View.extend({
+        className: "list-group-item",
+        tagName: "a",
+        initialize: function() {
+            this.template = Handlebars.compile($("#sounds-template").html()),
+            console.log('SoundView.initialize');
+            this.render();
+        },
+        render: function() {
+            console.log('Soundview.render : ');
+            var html = this.template(this.model.toJSON());
+            this.$el.html(html);
+            this.$el.addClass("disabled");
+            // this.$el.attr("href", this.model.attributes.url).removeClass("disabled");
+        }
+    });
+
+    // Collection de sons
+    var SoundsCollection = Backbone.Collection.extend({
+        url: "http://localhost/dictrew/server/allsounds.json",
+        initialize: function() {
+            console.log('SoundsCollection.initalize');
+            console.log('this.url : ', this.url);
+        }
+    });
+
+
+
+
+    // INSTANCIATIONS
+    var soundsCollection = new SoundsCollection();
+
+    var soundsView = new SoundsView({
+        el: '.js-list-sounds',
+        collection: soundsCollection
+    });
+
+    // soundsCollection.fetch().done(function(){
+    //      console.log('List found ');
+    //  });
 });
 
 // require(['./Overrides', './NavView', './Router'], function(Overrides, NavView, Router) {
-    // new NavView();
-    // new Router();
+// new NavView();
+// new Router();
 
-    // Backbone.history.start();
+// Backbone.history.start();
 // });
-
-
